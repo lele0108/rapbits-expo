0c6547e5f5856262214f73a944acdc2173268a0c
Jimmy Liu <sicong.liu98@gmail.com>
2017-04-18 23:09:37 -0700

    audio is in state system now

--- BitScreen.js
+++ BitScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   Text,
   View,
+  StyleSheet,
+  Image,
 } from 'react-native';
 import { Router } from './main';
 import Expo, {Asset, Audio, Font} from 'expo';
@@ -16,22 +18,37 @@ export default class BitScreen extends React.Component {
     }
   }
 
-  async componentDidMount() {
+  state = {
+    loading: true,
+    error: false,
+    sound: null,
+  };
+
+  componentDidMount() {
     Audio.setIsEnabledAsync(true);
+    this.loadAudio();
+  }
+
+  componentWillUnmount() {
+    this.state.sound.stopAsync();
+  }
+
+  loadAudio = async() => {
     const sound = new Expo.Audio.Sound({
       source: this.props.route.params.currentBit.mp3Snippet,
     });
     await sound.loadAsync();
     await sound.playAsync();
+    this.setState({sound});
   }
 
   render() {
-
-      console.log(this.props.route.params);
+    const rapbit = this.props.route.params.currentBit;
     return (
       <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-        <Text>{this.props.route.params.currentBit.lyric}</Text>
-        <Text>{this.props.route.params.currentBit.artistName}</Text>
+        <Image style={styles.coverPhoto} source={{ uri: rapbit.albumCover }} />
+        <Text>{rapbit.lyric}</Text>
+        <Text>{rapbit.artistName}</Text>
       </View>
     )
   }
@@ -40,3 +57,13 @@ export default class BitScreen extends React.Component {
     this.props.navigator.push('home');
   }
 }
+
+const styles = StyleSheet.create({
+  coverPhoto: {
+    width: 75,
+    height: 75,
+    marginBottom: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
